c54d4ae12efb5fe89ad12c772442fe912b29f26b
lebrancconvas <lebrancconvas@gmail.com>
2023-09-08 15:10:32 +0700

    ✨ feat: updates

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { app } from './renderer';
+import { PIXI, app, Graphics } from './renderer';
 import { config } from './config';
+
 
 document.body.appendChild(app.view);
 app.renderer.background.color = config.backgroundColor;
 
 // Init.
+const rect = new Graphics();
+rect
+  .beginFill(0x00AA00)
+  .lineStyle(3, 0xFF0000)
+  .drawRect(0, 0, 64, 64)
+  .endFill();
+
+app.stage.addChild(rect);
 
 // Game Loop.
---
+++ src/renderer/pixi.ts
@@ -0,0 +1,5 @@
+import * as PIXI from 'pixi.js';
+
+export const Graphics = PIXI.Graphics;
+
+export { PIXI };
